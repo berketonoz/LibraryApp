261955fc6c4cebecfcc8fe3b84410a133b0d135f
berketonoz <btonoz@sabanciuniv.edu>
2024-11-29 15:20:37 +0300

    Add Sequelize models for Book, Borrow, and User

---
+++ models/Borrow.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+
+const defineBorrow = (sequelize) => {
+  return sequelize.define(
+    "Borrow",
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      user_id: { type: DataTypes.INTEGER, allowNull: false },
+      book_id: { type: DataTypes.INTEGER, allowNull: false },
+      borrow_date: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+      return_date: { type: DataTypes.DATE, defaultValue: null },
+      user_score: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+          min: 1,
+          max: 10,
+        },
+      },
+    },
+    {
+      tableName: "borrows",
+      timestamps: false, // Disable createdAt and updatedAt
+    }
+  );
+}
+
+export default defineBorrow;
