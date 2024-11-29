585c1ffd5d5e8958f365a1e11d7d1f9b12f678ea
berketonoz <btonoz@sabanciuniv.edu>
2024-11-29 12:27:03 +0300

    Refactor API to retrieve user data and update collection handling

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-const dataPath = path.join(
+const collectionPath = path.join(
   __dirname,
   "./Library Case API Collection.postman_collection.json"
 );
-const apiCollection = JSON.parse(fs.readFileSync(dataPath, "utf8"));
+const usersPath = path.join(__dirname, "./data/Users.json");
+const collection = JSON.parse(fs.readFileSync(collectionPath, "utf8"));
+const users = JSON.parse(fs.readFileSync(usersPath, "utf8"));
 
 app.get("/users", (req, res) => {
-  res.json(apiCollection);
+  res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users.find((user) => user.id === parseInt(id));
+  res.json(user);
 });
 
 app.get("/test/collection/:id", (req, res) => {
-    const id = req.params.id;
-    res.json(JSON.parse(apiCollection.item[id].response[0].body));
+  const id = req.params.id;
+  res.json(JSON.parse(collection.item[id].response[0].body));
 });
 
 app.listen(port, () => {
