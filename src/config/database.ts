3ebc5b5c235b8f5888976f603425d59af4360808
berketonoz <btonoz@sabanciuniv.edu>
2024-11-30 13:36:51 +0300

    Remove unused model imports and associations from database configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,4 @@
 import { Sequelize } from "sequelize";
-import User from "../models/User";
-import Book from "../models/Book.ts";
-import Borrow from "../models/Borrow.ts";
 
 const sequelize = new Sequelize("library_management", "root", "Joe17814..", {
   host: "localhost",
@@ -9,11 +6,5 @@ const sequelize = new Sequelize("library_management", "root", "Joe17814..", {
   logging: false,
 });
 
-
-// Define associations
-Borrow.belongsTo(User, { foreignKey: "user_id", as: "user" });
-Borrow.belongsTo(Book, { foreignKey: "book_id", as: "book" });
-User.hasMany(Borrow, { foreignKey: "user_id", as: "borrows" });
-Book.hasMany(Borrow, { foreignKey: "book_id", as: "borrows" });
 
 export default sequelize;
